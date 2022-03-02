c21853ee5ddd0bd3df305228d7b429b9b29e022e
caio souza <caiocbahia@hotmail.com>
2022-03-01 22:13:15 -0300

    Finished chapter 4

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -40,23 +40,45 @@ let questions = [
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
-startgame = () => {
+startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
+    availableQuestions = [ ... questions];
+    console.log(availableQuestions)
+    getNewQuestion()
 };
 
 getNewQuestion = () => {
 
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+      return window.location.assign("/end.html");
+    }
+
     questionCounter++;
-    Math.floor(Math.random() = availableQuestions.length);
-    currentQuestion = availableQuestions(questionIndex);
-    questions.innerText = currentQuestion.question
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
         const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' * number]
+        choice.innerText = currentQuestion['choice' + number]
     })
+
+    availableQuestions.splice(questionIndex, 1)
+
+    acceptingAnswers = true;
 }
+
+choices.forEach(choice => {
+  choice.addEventListener('click', e =>{
+    if(!acceptingAnswers) return
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target 
+    const selectedAnswer = selectedChoice.dataset['number']
+
+    getNewQuestion()
+  })
+})
 
 startGame();
